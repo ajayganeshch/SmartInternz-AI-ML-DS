223bf98a800b1bab8470596de8b144125aad412d
Ajay Ganesh <ajayganesh789@gmail.com>
2023-11-21 04:49:58 +0530

    Updated

--- app.py
+++ app.py
@@ -12,31 +12,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('watch_prediction.html')
 
 @app.route('/predict.html', methods=['GET'])
 def predict():
    return render_template('predict.html')
 
-@app.route("/predict",methods = ['POST'])
+@app.route("/predict", methods=['POST'])
 def predict1():
    
    brand = request.form['Brand']
    model = request.form['Model']
    os = request.form['OS']
    connectivity = request.form['Connectivity']
-   displayType = request.form['DisplayType']
+   displayType = request.form['Display Type']
    resolution = request.form['Resolution']
-   displaySize = request.form['DisplaySize']
-   hrm = request.form['HRM']
-   waterresistance = request.form['waterResistance']
-   batterylife = request.form['batteryLife']
+   displaySize = request.form['Display Size']
+   hrm = request.form['Heart Rate Monitor']
+   waterresistance = request.form['Water Resistance']
+   batterylife = request.form['Battery Life']
    gps = request.form['GPS']
    nfc = request.form['NFC']
 
    variables = [brand, model, os,connectivity,displayType,displaySize,resolution,waterresistance,batterylife,hrm,gps,nfc]
 
-   x = pd.DataFrame([variables], columns=['Brand', 'Model', 'OS', 'Connectivity', 'DisplayType', 'DisplaySize', 'Resolution', 'waterResistance', 'batteryLife', 'HRM', 'GPS', 'NFC'])
+   x = pd.DataFrame([variables], columns=['Brand', 'Model', 'OS', 'Connectivity', 'Display Type', 'Display Size', 'Resolution', 'Water Resistance', 'Battery Life', 'Heart Rate Monitor', 'GPS', 'NFC'])
 
    x_scaled = scalar.transform(x)
    output = main_model.predict(x_scaled)
@@ -46,4 +46,4 @@ def predict1():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3090)
+    app.run(port=3090)
